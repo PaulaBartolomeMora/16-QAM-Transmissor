e5b76b237a30fefe98eeea500d467bf8a4a9d461
pbartolome <pbartolome@minsait.com>
2023-11-16 12:05:22 +0100

    qam_map

---
+++ QAM_ZP/QAM_ZP.srcs/sources_1/new/mapeado_16QAM_I.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.11.2023 11:55:25
+-- Design Name: 
+-- Module Name: mapeado_16QAM_I - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mapeado_16QAM_I is
+--  Port ( );
+end mapeado_16QAM_I;
+
+architecture Behavioral of mapeado_16QAM_I is
+
+begin
+
+
+end Behavioral;
