48c8b0e73c1bd6d9552df6c4bd08d3bb8257c5d5
pbartolome <pbartolome@minsait.com>
2024-01-04 09:32:58 +0100

    actualizados bloques anteriores, fin chapters

--- map_qam/map_qam.srcs/sim_1/new/tb_mapeado_16QAM.vhd
+++ map_qam/map_qam.srcs/sim_1/new/tb_mapeado_16QAM.vhd
@@ -1,10 +1,9 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_TEXTIO.ALL;
 use STD.TEXTIO.ALL;
+
 
 entity tb_qam16_wrapper is
 end tb_qam16_wrapper;
@@ -46,7 +45,7 @@ architecture tb of tb_qam16_wrapper is
     constant clk_period : time := 5.208333 ns; -- Periodo de 192 MHz base para entrada y salida
     signal cont_in : integer := 0; --contador de lectura de la señal de entrada
     file file_handle : text;
-    file file_handle2 : text;
+    file file_handle_I, file_handle_Q : text;
     signal eof : boolean := false;  
 
     signal TbClock : std_logic := '0';
@@ -73,12 +72,15 @@ begin
 
     TbClock <= not TbClock after clk_period/2 when TbSimEnded /= '1' else '0';
     clk <= TbClock;
---    clk <= not clk after clk_period/2;
-
 
     process(clk) --192MHz
+    --lectura datos FIFO
     variable line_buffer : line;
     variable nuevo_valor : STD_LOGIC_VECTOR(11 DOWNTO 0);
+    
+    --escritura datos FIR
+    variable line_buffer_I, line_buffer_Q : line;
+    variable nuevo_valor_I, nuevo_valor_Q : integer;
     
     begin
         if rst_0 = '1' then
@@ -95,187 +97,44 @@ begin
                         readline(file_handle, line_buffer);
                         read(line_buffer, nuevo_valor);
                         entrada <= nuevo_valor;
-                        --s_axis_data_tvalid_0 <= '1'; 
-                        --s_axis_data_tvalid_1 <= '1';
                     end if;
                 end if;
+            end if; 
+            if s_axis_data_tvalid_0 = '1' and s_axis_data_tvalid_1 = '1' and not eof then --escritura de salida de filtros FIR a 192MHz
+                --escritura de salida FIR I
+                nuevo_valor_I := to_integer(signed(m_axis_data_tdata_0));
+                write(line_buffer_I, nuevo_valor_I); 
+                writeline(file_handle_I, line_buffer_I); 
+                --escritura de salida FIR Q
+                nuevo_valor_Q := to_integer(signed(m_axis_data_tdata_1));
+                write(line_buffer_Q, nuevo_valor_Q); 
+                writeline(file_handle_Q, line_buffer_Q);
             end if;                      
         end if;
     end process;   
+
+    process
+    begin
     
-
-
-     process
-     begin
-        file_open(file_handle, "C:\Users\Paula\Downloads\FIFO_output.txt", READ_MODE);
+       file_open(file_handle, ".\FIFO_output.txt", READ_MODE);
+       --file_open(file_handle, ".\QAM_output.txt", WRITE_MODE); --salida mapeado+ZP
+       file_open(file_handle_I, "C:\Users\Paula\Downloads\FIR_output_I.txt", WRITE_MODE);
+       file_open(file_handle_Q, "C:\Users\Paula\Downloads\FIR_output_Q.txt", WRITE_MODE);
         
-        ce <= '0';
-        rst_0 <= '1';
+       ce <= '0';
+       rst_0 <= '1';        
+      
+       s_axis_data_tvalid_0 <= '0';
+       s_axis_data_tvalid_1 <= '0';
         
-        --entrada <= (others => '0');
-        s_axis_data_tvalid_0 <= '0';
-        s_axis_data_tvalid_1 <= '0';
-        
-        wait for 32*clk_period;
-        rst_0 <= '0';
-        wait for 32*clk_period;
-        ce <= '1';
-
-
-        --wait for 100 * clk_period;
-        wait;
-    end process;  
+       wait for 32*clk_period;
+       rst_0 <= '0';
+       wait for 32*clk_period;
+       ce <= '1';
+       s_axis_data_tvalid_0 <= '1';
+       s_axis_data_tvalid_1 <= '1';  
+              
+       wait;
+   end process;  
     
 end tb;
-
-
---entity tb_mapeado_16QAM IS
---end tb_mapeado_16QAM;
-
---architecture testbench of tb_mapeado_16QAM is
---    signal clk, ce, rst : STD_LOGIC := '0';
---    signal entrada : STD_LOGIC_VECTOR(11 DOWNTO 0) := (others => '0');
---    signal salida_I : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
---    signal salida_Q : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
---    signal s_axis_data_tvalid_0, s_axis_data_tvalid_1 : STD_LOGIC;
---    signal s_axis_data_tready_0, s_axis_data_tready_1 : STD_LOGIC;
---    signal m_axis_data_tvalid_0, m_axis_data_tvalid_1 : STD_LOGIC;
---    signal m_axis_data_tdata_0, m_axis_data_tdata_1 : STD_LOGIC_VECTOR ( 23 downto 0 );
-    
-
---    component mapeado_16QAM_I
---        port(
---            clk : IN STD_LOGIC;
---            ce, rst : IN STD_LOGIC;
---            entrada : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
---            salida : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
---        );
---    end component;
-    
---    component mapeado_16QAM_Q
---        port(
---            clk : IN STD_LOGIC;
---            ce, rst : IN STD_LOGIC;
---            entrada : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
---            salida : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
---        );
---    end component;
-    
---  component qam16_fir_compiler_0_0 is
---    port (
---            aclk : in STD_LOGIC;
---            s_axis_data_tvalid : in STD_LOGIC;
---            s_axis_data_tready : out STD_LOGIC;
---            s_axis_data_tdata : in STD_LOGIC_VECTOR ( 7 downto 0 );
---            m_axis_data_tvalid : out STD_LOGIC;
---            m_axis_data_tdata : out STD_LOGIC_VECTOR ( 23 downto 0 )
---    );
---    end component qam16_fir_compiler_0_0;
-    
---    component qam16_fir_compiler_0_1 is
---    port (
---            aclk : in STD_LOGIC;
---            s_axis_data_tvalid : in STD_LOGIC;
---            s_axis_data_tready : out STD_LOGIC;
---            s_axis_data_tdata : in STD_LOGIC_VECTOR ( 7 downto 0 );
---            m_axis_data_tvalid : out STD_LOGIC;
---            m_axis_data_tdata : out STD_LOGIC_VECTOR ( 23 downto 0 )
---    );
---    end component qam16_fir_compiler_0_1;
-    
-    
---    constant clk_period : time := 5.208333 ns; -- Periodo de 192 MHz base para entrada y salida
---    signal cont_in : integer := 0; --contador de lectura de la señal de entrada
---    file file_handle : text;
---    file file_handle2 : text;
---    signal eof : boolean := false;
-
-
---begin
---    qam_i: mapeado_16QAM_I port map(clk=>clk, ce=>ce, rst=>rst, entrada=>entrada, salida=>salida_I);
---    qam_q: mapeado_16QAM_Q port map(clk=>clk, ce=>ce, rst=>rst, entrada=>entrada, salida=>salida_Q);
---    fir_i: qam16_fir_compiler_0_0 port map(
---            aclk=>clk, 
---            s_axis_data_tvalid=>s_axis_data_tvalid_0,
---            s_axis_data_tready=>s_axis_data_tready_0, 
---            s_axis_data_tdata=>salida_I,
---            m_axis_data_tvalid=>m_axis_data_tvalid_0, 
---            m_axis_data_tdata=>m_axis_data_tdata_0);
-
---    fir_q: qam16_fir_compiler_0_1 port map(
---            aclk=>clk, 
---            s_axis_data_tvalid=>s_axis_data_tvalid_1,
---            s_axis_data_tready=>s_axis_data_tready_1, 
---            s_axis_data_tdata=>salida_Q,
---            m_axis_data_tvalid=>m_axis_data_tvalid_1, 
---            m_axis_data_tdata=>m_axis_data_tdata_1);
-
---    clk <= not clk after clk_period/2;
-
---    process(clk) --192MHz
---    variable line_buffer : line;
---    variable nuevo_valor : STD_LOGIC_VECTOR(11 DOWNTO 0);
-    
---    --escritura
---    variable line_num: line;
---    file outfile: text is out "C:\Users\Miguel\Downloads\QAM_output.txt";
-    
---    begin
---        if rst = '1' then
---            eof <= false; --se reinicia fin de archivo
---            cont_in <= 0;
---        elsif rising_edge(clk) then
---            cont_in <= cont_in + 1;
---            if cont_in = 96 then --cada 500 ns se lee entrada (cont = 96)
---                cont_in <= 0;
---                if not eof then
---                    if endfile(file_handle) then 
---                        eof <= true; 
---                    else
---                        readline(file_handle, line_buffer);
---                        read(line_buffer, nuevo_valor);
---                        entrada <= nuevo_valor;
---                        s_axis_data_tvalid_0 <= '1'; 
---                        s_axis_data_tvalid_1 <= '1';
---                    end if;
---                end if;
---            end if; 
---            if not eof then
---                write (line_num, salida_I);
---                writeline(outfile, line_num);
---            end if;                           
---        end if;
---    end process;   
-
---    process
---    begin
---        file_open(file_handle, "C:\Users\Miguel\Downloads\FIFO_output.txt", READ_MODE);
-        
---        --escritura
---        file_open (file_handle2, "C:\Users\Miguel\Downloads\QAM_output.txt", write_mode);
-        
---        -- Initialize inputs
---        ce <= '0';
---        rst <= '1';
---        --s_axis_data_tvalid_0 <= '0';
---        --s_axis_data_tvalid_1 <= '0';
-
-        
---        --s_axis_data_tdata_0 <= (others => '0');
---        --s_axis_data_tdata_1 <= (others => '0');
---        wait for 32*clk_period;
---        rst <= '0';
-        
---        wait for 32*clk_period;
---        ce <= '1';
---        s_axis_data_tvalid_0 <= '1';
---        s_axis_data_tvalid_1 <= '1';
---        --s_axis_data_tready_0 <= '1';
---        --s_axis_data_tready_1 <= '1';
-        
---        wait;
---    end process stim_proc;
-
---end testbench;
-
-
